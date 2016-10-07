164400596a4b5ea9287029044523bc0c049f2911
Seong-Joong Kim <seongjoong_kim2@tmax.co.kr>
2016-10-07 17:20:38 +0900

    Modified load/store operation considering acquire/release memory model

--- include/atomic.h
+++ include/atomic.h
@@ -13,100 +13,119 @@
 typedef int64_t atomic_t;
 
 
-static inline long
-atomic_load(volatile atomic_t *vp)
+/*
+ *	Atomic Load operation which consider x86 64-bit memory model
+ */
+static inline atomic_t 
+atomic_load(volatile atomic_t *ptr)
 {
-	return *vp;
+	return *ptr;
 }
 
-static inline long
-atomic_load_seq_cst(volatile atomic_t *vp)
+static inline atomic_t 
+atomic_load_acquire(volatile atomic_t *ptr)
+{
+	atomic_t value = *ptr;
+	asm __volatile__("mfence" ::: "memory");
+	return value;
+}
+
+static inline atomic_t 
+atomic_load_release(volatile atomic_t *ptr)
 {
 	asm __volatile__("mfence" ::: "memory");
-	return *vp;
+	return *ptr;
 }
 
 
 static inline void
-atomic_store(volatile atomic_t *vp, long nv)
+atomic_store(volatile atomic_t *ptr, atomic_t value)
 {
-	*vp = nv;
+	*ptr = value;
 }
 
 static inline void
-atomic_store_seq_cst(volatile atomic_t *vp, long nv)
+atomic_store_acquire(volatile atomic_t *ptr, atomic_t value)
 {
-	asm __volatile__("mfence" ::: "memory");
-	*vp = nv;
+	*ptr = value;
 	asm __volatile__("mfence" ::: "memory");
 }
 
-static inline long
-atomic_fetch_and_add(volatile atomic_t *vp, long nv)
+static inline void
+atomic_store_release(volatile atomic_t *ptr, atomic_t value)
+{
+	asm __volatile__("mfence" ::: "memory");
+	*ptr = value;
+}
+
+
+
+static inline atomic_t
+atomic_fetch_and_add(volatile atomic_t *ptr, atomic_t value)
 {
 	asm __volatile__(
 		"lock; xaddl %%eax, %2;"
-	 	:"=a" (nv)
-		:"a" (nv), "m" (*vp)
+	 	:"=a" (value)
+		:"a" (value), "m" (*ptr)
 		:"cc", "memory"
 	);
-	return nv;
+	return value;
 }
 
 
 static inline void
-atomic_add(volatile atomic_t *vp, long v)
+atomic_add(volatile atomic_t *ptr, atomic_t value)
 {
     asm __volatile__(
         "lock; addq %1,%0"
-        :"=m" (*vp)
-        :"ir" (v), "m" (*vp)
+        :"=m" (*ptr)
+        :"ir" (value), "m" (*ptr)
     );
 }
 
 
 static inline void
-atomic_sub(volatile atomic_t *vp, long v)
+atomic_sub(volatile atomic_t *ptr, atomic_t value)
 {
     asm __volatile__(
         "lock; subq %1,%0"
-        :"=m" (*vp)
-        :"ir" (v), "m" (*vp)
+        :"=m" (*ptr)
+        :"ir" (value), "m" (*ptr)
     );
 }
 
 static inline void
-atomic_inc(volatile atomic_t *vp)
+atomic_inc(volatile atomic_t *ptr)
 {
     asm __volatile__(
         "lock; incq %0"
-        :"=m" (*vp)
-        :"m" (*vp)
+        :"=m" (*ptr)
+        :"m" (*ptr)
     );
 }
 
 static inline void
-atomic_dec(volatile atomic_t *vp)
+atomic_dec(volatile atomic_t *ptr)
 {
     asm __volatile__(
         "lock; decq %0"
-        :"=m" (*vp)
-        :"m" (*vp)
+        :"=m" (*ptr)
+        :"m" (*ptr)
     );
 }
 
 
-static inline long
-atomic_cas(volatile atomic_t *vp, long nv, long ov)
+static inline atomic_t
+atomic_cas(volatile atomic_t *ptr, atomic_t new_value, atomic_t old_value)
 {
     asm __volatile__(
         "lock; cmpxchgq %1,(%3)"
-        : "=a" (nv)
-        : "r" (nv), "a" (ov), "r" (vp)
+        : "=a" (new_value)
+        : "r" (new_value), "a" (old_value), "r" (ptr)
         : "cc", "memory"
     );
 
-    return nv;
+    return new_value;
 }
 
 #endif
